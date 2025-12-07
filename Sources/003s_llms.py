30e5dd2385d30cfc5a036d5e74a4ce49816f588a
Utkan Başurgan <140505686+utkanbasurgan@users.noreply.github.com>
2025-12-07 15:14:05 +0300

    Update 003s_llms.py

--- Sources/003s_llms.py
+++ Sources/003s_llms.py
@@ -2,7 +2,7 @@
 # Utkan Başurgan
 #
 #---------------------------------------------------------------------------------------------------------------------------------
-abcdef
+abcdefg
 import torch
 import torch.nn as nn
 import torch.optim as optim
